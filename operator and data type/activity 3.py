1de536d3f83a3af9dcacee43c5d1e577050e5652
tammy-245678 <abdullahbigman38@gmail.com>
2024-12-14 19:47:05 +0500

    commit

---
+++ operator and data type/activity 3.py
@@ -0,0 +1,9 @@
+x=int(input("enter first number"))
+y=int(input("enter second  number"))
+temp=x
+x=y
+y=temp
+print("value of x after swapping",x)
+print("value of y after swapping",y)
+
+
---
+++ operator and data type/name.py
@@ -0,0 +1,20 @@
+name="Hania"
+age=17
+weight=54.6
+student=True
+print((name))
+print((age))
+print((weight))
+print((student))
+
+num1=10
+num2=20
+print("addition",num1+num2)
+print("subtractioon",num2-num1)
+print("multiplication",num2*num1)
+print("division",num2/num1)
+print("modulus",num1%num2)
+print("exponent",num1**num2)
+print("floor division",num1//num2)
+
+
